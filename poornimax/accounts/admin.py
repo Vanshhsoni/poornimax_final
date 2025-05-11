77828b4f9a2357ecbde3ad1bb380be293f7de3d9
Vanshhsoni <sonivanshmaster@gmail.com>
2025-05-11 19:39:53 +0530

    x

--- poornimax/accounts/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth import get_user_model
-
-# Get your custom User model
-User = get_user_model()
-
-# Register the User model to the admin panel
-admin.site.register(User)
--- poornimax/poornima_site/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PoornimaSiteConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'poornima_site'
